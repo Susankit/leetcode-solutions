1d26160c2552bc5d6d905032cd11cae501f47841
Susankit <susankitkumarjha1@gmail.com>
2026-08-10 18:54:22 +0530

    Time: 3 ms (58.53%), Space: 121.9 MB (39.48%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -24,6 +24,9 @@ public:
     }
 
     bool isPalindrome(ListNode* head) {
+        if (head == NULL || head->next == NULL) {
+            return true;
+        }
         ListNode* slow = head;
         ListNode* fast = head;
         while (fast->next != NULL && fast->next->next != NULL) {
